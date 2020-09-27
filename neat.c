19575d2711335867975f05f8665b77feb2b25092
DHBW-TEL19GR2-et19036 <et19036@lehre.dhbw-stuttgart.de>
2020-09-27 21:05:30 +0200

    neat functions

--- neat.c
+++ neat.c
@@ -10,8 +10,15 @@ typedef struct {
 	float** layers;
 } NeuralNet;
 
+typedef struct {
+	int size;
+	NeuralNet** people;
+} Population;
+
+// Mathematical help functions
 void random_vector(float* vector, int size);
 
+// Neural net functions
 NeuralNet create_neural_net(int num_layers, int* topogoly);
 void delete_neural_net(NeuralNet* net); 
 void predict(NeuralNet* net, float* input, float* output);
@@ -19,16 +26,23 @@ void mutate_weights(NeuralNet* net, float mutation_rate);
 NeuralNet* copy_weights(NeuralNet* dest, NeuralNet* src);
 NeuralNet copy_neural_net(NeuralNet* net);
 
+// NEAT functions
+Population new_population(int size);
+void delete_population(Population* population);
+void calculate_reward();
+Population inherit_population(Population precesters);
+
+
 int main(void) {
 	srand(time(NULL));
 
-	float input[] = {1.0f, 1.0f, 1.0f};
-	int topology[] = {3, 2, 2};
-	NeuralNet net = create_neural_net(3, topology);
+	float input[] = {1.0f, 1.0f};
+	/*int topology[] = {2, 1};
+	NeuralNet net = create_neural_net(2, topology);
 
-	float output[2];
+	float output[1];
 	predict(&net, input, output);
-	printf("result: %10f%10f\n", output[0], output[1]);
+	printf("result: %10f\n", output[0]);
 
 	mutate_weights(&net, 0.5f);
 	predict(&net, input, output);
@@ -38,7 +52,14 @@ int main(void) {
 	predict(&net2, input, output);
 	printf("result: %10f%10f\n", output[0], output[1]);
 
-	delete_neural_net(&net);
+	delete_neural_net(&net);*/
+
+	float output[1];
+	Population pop = new_population(2);
+	predict(pop.people[0], input, output);
+	printf("result: %10f\n", output[0]);
+
+	delete_population(&pop);
 
 	return 0;
 }
@@ -168,4 +189,25 @@ NeuralNet copy_neural_net(NeuralNet* net) {
 	}
 
 	return new_net;
+}
+
+
+Population new_population(int size) {
+	Population pop = {.size = size};
+	int topology[] = {2, 1};
+	pop.people = (NeuralNet**) malloc(sizeof(NeuralNet*) * size);
+	for (int i = 0; i < size; ++i) {
+		pop.people[i] = (NeuralNet*) malloc(sizeof(NeuralNet));
+		NeuralNet net = create_neural_net(2, topology);
+		memcpy(pop.people[i], &net, sizeof(NeuralNet));
+	}
+	return pop;
+}
+
+void delete_population(Population* population) {
+	for (int i = 0; i < population->size; ++i) {
+		delete_neural_net(population->people[i]);
+		free(population->people[i]);
+	}
+	free(population->people);
 }
