9c4579036f64d49e269c9a6b9b5381fa5e024543
DHBW-TEL19GR2-et19036 <et19036@lehre.dhbw-stuttgart.de>
2020-10-06 20:33:55 +0200

    update

--- neat.cpp
+++ neat.cpp
@@ -4,44 +4,82 @@
 #include <string.h>
 #include <math.h>
 #include <vector>
+#include <map>
 
 class NeuralNet {
     private:
-    std:vector<int> topology;
+    std::vector<int> topology;
     std::vector< std::vector <std::vector<float>>> weights;
+	std::vector< std::vector<float>> layers;
 
     public:
     NeuralNet(std::vector<int> topology): topology(topology) {
-
+		for (int i = 0; i < topology.size()-1; ++i) {
+			std::vector< std::vector<float>> weightlayer;
+			for (int j = 0; j < topology[i]; ++j) {
+				std::vector<float> w;
+				for (int k = 0; k < topology[i+1]; ++k) {
+					w.push_back( (float)( rand() )/(float)( RAND_MAX ) * 4.0f - 2.0f );
+				}
+				weightlayer.push_back(w);
+			}
+			weights.push_back(weightlayer);
+			std::vector<float> layer(topology[i]);
+			layers.push_back(layer);
+		}
+		// final layer (output)
+		layers.push_back(std::vector<float>(*(topology.end())));
     }
+
+	std::vector<float> predict(std::vector<float> input) {
+		for (int i = 0; i < this->layers.size() - 1; ++i) {
+			// zero next layer
+			std::fill(this->layers[i].begin(), this->layers[i].end(), 0.0f);
+			for (int j = 0; j < this->topology[i]; ++j) {
+				for (int k = 0; k < this->topology[i+1]; ++k) {
+					this->layers[i+1][k] += this->layers[i][j] * this->weights[i][j][k];
+				}
+			}
+		}
+		return std::vector(this->layers[*topology.end()]);
+	}
+
+	void mutate_weights(float rate) {
+		for (int i = 0; i < this->layers.size()-1; ++i) {
+			for (int j = 0; j < this->topology[i]; ++j) {
+				for (int k = 0; k < this->topology[i+1]; ++k) {
+					if ( (rand() % 10000) / 10000.0f < rate) {
+						this->weights[i][j][k] += 2.0f * ( rand() - RAND_MAX/2.0f) / RAND_MAX;
+					}
+				}
+			}
+		}
+	}
 };
 
-typedef struct {
-	int num_layers;
-	int* topology;
-	float*** weights;
-	float** layers;
-} NeuralNet;
+class Population {
+	private:
+	std::vector<NeuralNet> nets;
+	float average_fitness;
+	float best_fitness;
+	std::vector<float> fitness;
 
-typedef struct {
-	int size;
-	NeuralNet** people;
-} Population;
+	public:
+	Population(int size, std::vector<int> topology) {
+		for (int i = 0; i < size; ++i) {
+			nets.push_back(NeuralNet(topology));
+		}
+	}
 
-// Mathematical help functions
-void random_vector(float* vector, int size);
+	void next_generation(void) {
+		Population old_pop(*this);
 
-// Neural net functions
-NeuralNet create_neural_net(int num_layers, int* topogoly);
-void delete_neural_net(NeuralNet* net); 
-void predict(NeuralNet* net, float* input, float* output);
-void mutate_weights(NeuralNet* net, float mutation_rate);
-NeuralNet* copy_weights(NeuralNet* dest, NeuralNet* src);
-NeuralNet copy_neural_net(NeuralNet* net);
+
+	}
+};
+
 
 // NEAT functions
-Population new_population(int size);
-void delete_population(Population* population);
 float calculate_reward(float* output, float* goal, int vector_size);
 Population inherit_population(Population* precesters, float* chances_to_inherit);
 
@@ -109,151 +147,6 @@ int main(void) {
 	return 0;
 }
 
-void random_vector(float* vector, int size) {
-	for (int i = 0; i < size; ++i) {
-		vector[i] = (float)( rand() )/(float)( RAND_MAX ) * 2.0f - 1.0f;
-	}
-}
-
-
-NeuralNet create_neural_net(int num_layers, int* topology) {
-	NeuralNet net = {num_layers};
-	net.topology = (int*) malloc (sizeof(int) * num_layers);
-	memcpy(net.topology, topology, num_layers*sizeof(int));
-	// Every layer except for the last has weights
-	net.weights = (float***) malloc(sizeof(float*) * num_layers-1);
-	net.layers = (float**) malloc(sizeof(float*) * num_layers);
-	for (int i = 0; i < num_layers; ++i) {
-		// Create neurons for this layer
-		float* l = (float*) calloc(topology[i], sizeof(float));
-		net.layers[i] = l;
-		
-		if (i == num_layers - 1) break;
-
-		// Create weights for this layer. Last layer cannot have weights
-		float** weight_matrix = (float**) malloc(sizeof(float*) * topology[i]);
-		net.weights[i] = weight_matrix;
-		for (int j = 0; j < topology[i]; ++j) {
-			float* weights_for_one_neuron = (float*) malloc(sizeof(float) * topology[i+1]);
-			random_vector(weights_for_one_neuron, topology[i+1]);
-			net.weights[i][j] = weights_for_one_neuron;
-		}
-	}
-
-	return net;
-}
-
-void delete_neural_net(NeuralNet* net) {
-	for (int i = 0; i < net->num_layers; ++i) {
-		free(net->layers[i]);
-		if (i == net->num_layers - 1) break;
-		for (int j = 0; j < net->topology[i]; ++j) {
-			free(net->weights[i][j]);
-		}
-		free(net->weights[i]);
-	}
-	free(net->layers);
-	free(net->weights);
-	free(net->topology);
-}
-
-
-void predict(NeuralNet* net, float* input, float* output) {
-	// copy input to first layer
-	memcpy(net->layers[0], input, sizeof(float) * net->topology[0]);
-	for (int i = 0; i < net->num_layers - 1; ++i) {
-		// zero next layer
-		memset(net->layers[i+1], 0, sizeof(float) * net->topology[i+1]);
-		for (int j = 0; j < net->topology[i]; ++j) {
-			for (int k = 0; k < net->topology[i+1]; ++k) {
-				net->layers[i+1][k] += net->layers[i][j] * net->weights[i][j][k];
-			}
-		}
-	}
-	memcpy(output, net->layers[net->num_layers-1], sizeof(float) * net->topology[net->num_layers-1]);
-}
-
-
-void mutate_weights(NeuralNet* net, float mutation_rate) {
-	for (int i = 0; i < net->num_layers-1; ++i) {
-		for (int j = 0; j < net->topology[i]; ++j) {
-			for (int k = 0; k < net->topology[i+1]; ++k) {
-				if ( (rand() % 10000) / 10000.0f < mutation_rate) {
-					net->weights[i][j][k] += 2.0f * ( rand() - RAND_MAX/2.0f) / RAND_MAX;
-				}
-			}
-		}
-	}
-}
-
-
-NeuralNet* copy_weights(NeuralNet* dest, NeuralNet* src) {
-	// check if nets have the same topology
-	if (dest->num_layers != src->num_layers) return NULL;
-	for (int i = 0; i < dest->num_layers; ++i) {
-		if (dest->topology[i] != src->topology[i]) return NULL;
-	}
-
-	// copy weights
-	for (int i = 0; i < dest->num_layers-1; ++i) {
-		for (int j = 0; j < dest->topology[i]; ++j) {
-			memcpy(dest->weights[i][j], src->weights[i][j], sizeof(float) * dest->topology[i+1]);
-		}
-	}
-	return dest;
-}
-
-
-NeuralNet copy_neural_net(NeuralNet* net) {
-	NeuralNet new_net = {.num_layers = net->num_layers};
-	
-	new_net.topology = (int*) malloc(sizeof(int) * net->num_layers);
-	memcpy(new_net.topology, net->topology, sizeof(int) * net->num_layers);
-	
-	new_net.weights = (float***) malloc(sizeof(float*) * net->num_layers - 1);
-	new_net.layers = (float**) malloc(sizeof(float*) * net->num_layers);
-	for (int i = 0; i < net->num_layers; ++i) {
-		// Create neurons for this layer
-		float* l = (float*) calloc(net->topology[i], sizeof(float));
-		new_net.layers[i] = l;
-		
-		if (i == new_net.num_layers - 1) break;
-
-		// Create weights for this layer. Last layer cannot have weights
-		float** weight_matrix = (float**) malloc(sizeof(float*) * new_net.topology[i]);
-		new_net.weights[i] = weight_matrix;
-		for (int j = 0; j < new_net.topology[i]; ++j) {
-			float* weights_for_one_neuron = (float*) malloc(sizeof(float) * new_net.topology[i+1]);
-			new_net.weights[i][j] = weights_for_one_neuron;
-		}
-	}
-	if (copy_weights(&new_net, net) == NULL) {
-		printf("Failed to copy net: failed to copy weights\n");
-		exit(1);
-	}
-
-	return new_net;
-}
-
-
-Population new_population(int size) {
-	Population pop = {.size = size};
-	int topology[] = {5, 2};
-	pop.people = (NeuralNet**) malloc(sizeof(NeuralNet*) * size);
-	for (int i = 0; i < size; ++i) {
-		pop.people[i] = (NeuralNet*) malloc(sizeof(NeuralNet));
-		*(pop.people[i]) = create_neural_net(2, topology);
-	}
-	return pop;
-}
-
-void delete_population(Population* population) {
-	for (int i = 0; i < population->size; ++i) {
-		delete_neural_net(population->people[i]);
-		free(population->people[i]);
-	}
-	free(population->people);
-}
 
 float calculate_reward(float* output, float* goal, int vector_size) {
 	// calculates reward like a sigmoid function
