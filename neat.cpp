3347548d20437e4b161919c2de537763b19a5c0e
DHBW-TEL19GR2-et19036 <et19036@lehre.dhbw-stuttgart.de>
2020-10-08 21:25:23 +0200

    update cpp

--- neat.cpp
+++ neat.cpp
@@ -28,20 +28,21 @@ class NeuralNet {
 			layers.push_back(layer);
 		}
 		// final layer (output)
-		layers.push_back(std::vector<float>(*(topology.end())));
+		layers.push_back(std::vector<float>(topology.back()));
     }
 
 	std::vector<float> predict(std::vector<float> input) {
+		this->layers[0] = std::vector<float>(input);
 		for (int i = 0; i < this->layers.size() - 1; ++i) {
 			// zero next layer
-			std::fill(this->layers[i].begin(), this->layers[i].end(), 0.0f);
+			std::fill(this->layers[i+1].begin(), this->layers[i+1].end(), 0.0f);
 			for (int j = 0; j < this->topology[i]; ++j) {
 				for (int k = 0; k < this->topology[i+1]; ++k) {
 					this->layers[i+1][k] += this->layers[i][j] * this->weights[i][j][k];
 				}
 			}
 		}
-		return std::vector(this->layers[*topology.end()]);
+		return std::vector<float>(this->layers[this->layers.size()-1]);
 	}
 
 	void mutate_weights(float rate) {
@@ -59,121 +60,113 @@ class NeuralNet {
 
 class Population {
 	private:
-	std::vector<NeuralNet> nets;
 	float average_fitness;
 	float best_fitness;
-	std::vector<float> fitness;
 
 	public:
+	std::vector<float> fitness;
+	std::vector<NeuralNet> nets;
 	Population(int size, std::vector<int> topology) {
 		for (int i = 0; i < size; ++i) {
 			nets.push_back(NeuralNet(topology));
 		}
+		this->fitness = std::vector<float>(size);
 	}
 
-	void next_generation(void) {
+	void next_generation(float mutation_rate) {
 		Population old_pop(*this);
-
-
+		float fitness_sum = 0.0f;
+		for (float f : this->fitness) {
+			fitness_sum += f;
+		}
+		for (int i = 0; i < this->fitness.size(); ++i) {
+			this->fitness[i] /= fitness_sum;
+		}
+		std::vector<float> chances_lut(this->nets.size());
+		chances_lut[0] = this->fitness[0];
+		for (int i = 1; i < this->fitness.size(); ++i) {
+			chances_lut[i] = chances_lut[i-1] + this->fitness[i];
+		}
+		for (int i = 0; i < this->fitness.size(); ++i) {
+			float random = (float) rand() / RAND_MAX;
+			int index = 0;
+			while (random > chances_lut[index]) ++index;
+			this->nets[i] = NeuralNet(old_pop.nets[index]);
+			this->nets[i].mutate_weights(mutation_rate);
+		}
 	}
 };
 
 
 // NEAT functions
-float calculate_reward(float* output, float* goal, int vector_size);
-Population inherit_population(Population* precesters, float* chances_to_inherit);
+float calculate_reward(std::vector<float> output, std::vector<float> goal);
 
 
 int main(void) {
 	srand(time(NULL));
 
-	float input[5] = {};
+	std::vector<float> input(3);
+	std::vector<float> input2(3);
+	std::vector<float> input3(3);
 
 	FILE* logfile = fopen("log.txt", "w");
 	fprintf(logfile, "#Generation\tBest fitness\n");
 
-	float output[2];
 	int popsize = 50;
 	int gensize = 100;
-	Population pop = new_population(popsize);
+	Population pop = Population(popsize, {3,2});
 	for (int gen = 0; gen < gensize; ++gen) {
 		float maxreward = 0.0f;
 		int best_person = 0;
-		for (int i = 0; i < 5; ++i) {
+		for (int i = 0; i < input.size(); ++i) {
 			input[i] = (float)rand() / RAND_MAX * 1000.0f - 500.0f;
+			input2[i] = (float)rand() / RAND_MAX * 1000.0f - 500.0f;
+			input3[i] = (float)rand() / RAND_MAX * 1000.0f - 500.0f;
 		}
-		float goal[2] = {input[0] + 2*input[1] + 3*input[2] + 4*input[3] + 5*input[4], input[3] - input[2]};
+		std::vector<float> goal = {input[0] + input[1] + input[2], 5*input[2] - input[1] - input[0]};
+		std::vector<float> goal2 = {input2[0] + input2[1] + input2[2], 5*input2[2] - input2[1] - input2[0]};
+		std::vector<float> goal3 = {input3[0] + input3[1] + input3[2], 5*input3[2] - input3[1] - input3[0]};
 		float gen_fitness = 0.0f;
-		float rewards[pop.size];
 		for (int i = 0; i < popsize; ++i) {
-			predict(pop.people[i], input, output);
-			float reward = calculate_reward(pop.people[i]->layers[1], goal, 2);
+			std::vector<float> output = pop.nets[i].predict(input);
+			std::vector<float> output2 = pop.nets[i].predict(input2);
+			std::vector<float> output3 = pop.nets[i].predict(input3);
+			float reward = calculate_reward(output, goal);
+			reward += calculate_reward(output2, goal2);
+			reward += calculate_reward(output3, goal3);
+			pop.fitness[i] = reward;
 			gen_fitness += reward;
-			rewards[i] = reward;
 			if (reward > maxreward) {
 				maxreward = reward;
 				best_person = i;
 			}
 		}
-		predict(pop.people[best_person], input, output);
-		printf("best fitness = %.4f from %d\n", maxreward, best_person);
 		fprintf(logfile, "%d\t%f\n", gen, maxreward);
-		float chances[pop.size];
-		for (int i = 0; i < pop.size; ++i) {
-			chances[i] = rewards[i] / gen_fitness;
-		}
 		if (gen < gensize-1) {
-			pop = inherit_population(&pop, chances);
+			pop.next_generation(0.167f);
 		} else {
-			printf("Testing input 5 4 3 2 1\n");
-			goal[0] = 0.0f;
-			goal[1] = 0.0f;
-			input[0] = 5;
-			input[1] = 4;
-			input[2] = 3;
-			input[3] = 2;
-			input[4] = 1;
-			goal[0] = input[0] + 2*input[1] + 3*input[2] + 4*input[3] + 5*input[4];
-			goal[1] = input[3] - input[2];
-			printf("Goal: %f %f\n", goal[0], goal[1]);
-			predict(pop.people[best_person], input, output);
+			printf("Testing input 1 1 1\n");
+			input = {1,1,1};
+			goal = {input[0] + input[1] + input[2], 5*input[2] - input[1] - input[0]};
+			printf("Goal: %f %f\n", input[0] + input[1] + input[2], 5*input[2] - input[1] - input[0]);
+			std::vector<float> output = pop.nets[best_person].predict(input);
 			printf("Output: %f %f\n", output[0], output[1]);
 		}
 	}
 
 	fclose(logfile);
-	delete_population(&pop);
 
 	return 0;
 }
 
 
-float calculate_reward(float* output, float* goal, int vector_size) {
-	// calculates reward like a sigmoid function
-	float reward = 0.0f;
-	for (int i = 0; i < vector_size; ++i) {
-		float r = 1.0f/fabs(goal[i] - output[i]);
-		if (r > 1.0f) r = 1.0f;
-		reward += r;
+float calculate_reward(std::vector<float> output, std::vector<float> goal) {
+	float reward = 1.0f;
+	for (int i = 0; i < output.size(); ++i) {
+		float r = 1.0f/fabs(goal[i]/output[i] - 1.0f);
+		if (r > 1000.0f) r = 1000.0f;
+		reward *= r;
 	}
+	reward = pow(reward, 1.0f/output.size());
 	return reward;
-}
-
-Population inherit_population(Population* precesters, float* chances_to_inherit) {
-	Population new_pop = new_population(precesters->size);
-	float chances_lut[precesters->size];
-	chances_lut[0] = chances_to_inherit[0];
-	for (int i = 1; i < precesters->size; ++i) {
-		chances_lut[i] = chances_lut[i-1] + chances_to_inherit[i];
-	}
-	for (int i = 0; i < precesters->size; ++i) {
-		float random = (float) rand() / RAND_MAX;
-		int index = 0;
-		while (random > chances_lut[index]) ++index;
-		delete_neural_net(new_pop.people[i]);
-		*(new_pop.people[i]) = copy_neural_net(precesters->people[index]);
-		mutate_weights(new_pop.people[i], 0.1);
-	}
-	delete_population(precesters);
-	return new_pop;
 }
